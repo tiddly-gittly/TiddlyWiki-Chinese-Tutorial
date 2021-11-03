092abb30f08737594ab3e56a10ee2731c0f196e9
oflg <oflg@foxmail.com>
2021-11-03 23:22:51 +0800

    update Fishing to v1.5.0

--- plugins/oflg/fishing/tiddlers/system/excisefishing.js
+++ plugins/oflg/fishing/tiddlers/system/excisefishing.js
@@ -14,20 +14,27 @@ exports['excisefishing'] = function (event, operation) {
     editTiddlerTitle = editTiddler.fields['draft.of'];
   }
   const fishingTag = event.paramObject.fishingTag || '?';
+  // add due, default due in one day
+  const due = new Date(new Date().getTime() + 86400000).toISOString().replace(/-|T|:|\.|Z/g, '');
+  // we add current time to legacy title, so won't collide with parent title
+  const currentTime = new Date(new Date().getTime()).toISOString().replace(/-|T|:|\.|Z/g, '');
 
   const excisionQuestion = this.wiki.generateNewTitle(operation.selection);
+  const title = excisionQuestion;
   const excisionAnswer = event.paramObject.selectionAsAnswer === 'yes' ? operation.selection : '';
-  // add due, default due in one day
-  const due = new Date(new Date().getTime() + 1).toISOString().replace(/-|T|:|\.|Z/g, "");
   // add template
-  const captionTemplate = event.paramObject.template ? `{{||${event.paramObject.template}}}` : '';
+  const captionTemplate = event.paramObject.template
+    ? `{{||${event.paramObject.template}}}`
+    : event.paramObject.randomCaption
+    ? `${editTiddlerTitle}/${currentTime}`
+    : '';
   this.wiki.addTiddler(
     new $tw.Tiddler(this.wiki.getCreationFields(), this.wiki.getModificationFields(), {
-      title: excisionQuestion,
+      title,
       text: excisionAnswer,
       tags: event.paramObject.tagnew === 'yes' ? [editTiddlerTitle, fishingTag] : [fishingTag],
-      due: due,
-      caption: captionTemplate
+      due,
+      caption: captionTemplate,
     })
   );
   // "?" is the default fishing macro
